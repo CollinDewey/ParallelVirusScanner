93f1ed5cc06255d1960f4d88d5f79d54744e6d9e
jtjohnson565 <jtjohnson565@gmail.com>
2024-12-01 19:15:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,26 @@ from scan import md5_scan
 
 logger = logging.getLogger(__name__)
 
-async def scan_dir(path, num_threads, mode, semaphore):
+#################################################################
+## Extracts Zip Archive to Temporary Directory
+def extract_zip(zip_path, tmp_dir):
+    try:
+        logger.info(f"Detected ZIP file: {zip_path}")
+        # Save the ZIP file to the TMP directory
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            tmp_zip_path = os.path.join(tmp_dir, os.path.basename(zip_path))
+            logger.info(f"Extracting ZIP file: {zip_path} into TMP directory")
+            zip_ref.extractall(tmp_zip_path)
+        logger.info(f"Extraction complete for: {zip_path}")
+        return True
+    except Exception as e:
+        logger.error(f"Error processing ZIP file: {zip_path}. Exception: {e}")
+        return False
+    
+
+#################################################################
+## Scans Directory with Block Scheduling
+async def scan_dir_with_block(path, num_threads, mode, semaphore, true_path):
     tasks = []
     tmp_dir = tempfile.mkdtemp()
     scan_tmp = False
@@ -21,23 +40,16 @@ async def scan_dir(path, num_threads, mode, semaphore):
         for file in files:
 
             if zipfile.is_zipfile(os.path.join(root, file)):
-                    try:
-                        logger.info(f"Detected ZIP file: {os.path.join(root, file)}")
-                        # Save the ZIP file to the TMP directory
-                        with zipfile.ZipFile(os.path.join(root, file), 'r') as zip_ref:
-                            tmp_zip_path = os.path.join(tmp_dir, os.path.basename(file))
-                            logger.info(f"Extracting ZIP file: {os.path.join(root, file)} into TMP directory")
-                            zip_ref.extractall(tmp_zip_path)
-                        logger.info(f"Extraction complete for: {os.path.join(root, file)}")
-                        scan_tmp = True
-                    except Exception as e:
-                        logger.error(f"Error processing ZIP file: {os.path.join(root, file)}. Exception: {e}")
+                is_extracted = extract_zip(os.path.join(root, file), tmp_dir)
+                
+                if scan_tmp == False and is_extracted == True:
+                    scan_tmp = True
             else:
-                tasks.append(asyncio.create_task(scan_file(os.path.join(root, file), mode, semaphore)))
+                tasks.append(asyncio.create_task(scan_file(os.path.join(root, file), mode, semaphore, true_path)))
     
     if scan_tmp:
-        logger.info(f"Scanning {tmp_dir}")
-        await scan_dir(tmp_dir, num_threads, mode, semaphore)
+        logger.info(f"Scanning extracted files in TMP directory: {tmp_dir}")
+        await scan_dir_with_block(tmp_dir, num_threads, mode, semaphore, true_path)
 
     await asyncio.gather(*tasks)
 
@@ -48,7 +60,10 @@ async def scan_dir(path, num_threads, mode, semaphore):
         except Exception as e:
             logger.error(f"Error deleting TMP directory: {e}")
 
-async def scan_dir_with_cycle_scheduling(path, num_threads, mode):
+
+#################################################################
+## Scans Directory with Cycle Scheduling
+async def scan_dir_with_cycle_scheduling(path, num_threads, mode, true_path):
     tmp_dir = tempfile.mkdtemp()
     scan_tmp = False
 
@@ -58,31 +73,23 @@ async def scan_dir_with_cycle_scheduling(path, num_threads, mode):
 
     for root, dirs, files in os.walk(path):
         for file in files:
-            file_path = os.path.join(root, file)
-
-            if zipfile.is_zipfile(file_path):
-                try:
-                    logger.info(f"Detected ZIP file: {file_path}")
-                    # Extract ZIP file into the TMP directory
-                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
-                        logger.info(f"Extracting ZIP file: {file_path} into TMP directory")
-                        zip_ref.extractall(tmp_dir)
-                    logger.info(f"Extraction complete for: {file_path}")
+            if zipfile.is_zipfile(os.path.join(root, file)):
+                is_extracted = extract_zip(os.path.join(root, file), tmp_dir)
+                
+                if scan_tmp == False and is_extracted == True:
                     scan_tmp = True
-                except Exception as e:
-                    logger.error(f"Error processing ZIP file: {file_path}. Exception: {e}")
             else:
                 worker_id = next(workers)  # Assign task to the next worker in cycle
-                tasks_per_worker[worker_id].append(file_path)
+                tasks_per_worker[worker_id].append(os.path.join(root, file))
 
     if scan_tmp:
         logger.info(f"Scanning extracted files in TMP directory: {tmp_dir}")
-        await scan_dir_with_cycle_scheduling(tmp_dir, num_threads, mode)
+        await scan_dir_with_cycle_scheduling(tmp_dir, num_threads, mode, true_path)
 
     # Run tasks for each worker
     semaphores = [asyncio.Semaphore(1) for _ in range(num_threads)]
     worker_tasks = [
-        asyncio.create_task(scan_worker(tasks, mode, semaphores[i]))
+        asyncio.create_task(scan_worker(tasks, mode, semaphores[i], true_path))
         for i, tasks in tasks_per_worker.items()
     ]
     await asyncio.gather(*worker_tasks)
@@ -95,35 +102,40 @@ async def scan_dir_with_cycle_scheduling(path, num_threads, mode):
             logger.error(f"Error deleting TMP directory: {e}")
 
 
-async def scan_worker(tasks, mode, semaphore):
-    # Worker to process a list of files
+#################################################################
+## Worker to process a list of files
+async def scan_worker(tasks, mode, semaphore, true_path):
     for task in tasks:
-        await scan_file(task, mode, semaphore)
+        await scan_file(task, mode, semaphore, true_path)
 
 
-async def scan_file(path, mode, semaphore):
+#################################################################
+## Scans a file for a virus using a database of md5 hashes
+async def scan_file(path, mode, semaphore, true_path):
     async with semaphore:
         logger.info(f"Scanning file: {path}")
         try:
             if os.path.isfile(path):
                 async with aiofiles.open(path, "rb") as file:
                     match mode.lower():
-                        case 'all':
+                        case 'dir':
                             if await md5_scan(file):
-                                print(f"Virus found: {path}")
-                        case 'md5':
-                            if await md5_scan(file):
-                                print(f"Virus found: {path}")
+                                print(f"Virus found inside directory \"{true_path}\": {os.path.basename(path)}")
                         case 'zip':
-                            pass
+                            if await md5_scan(file):
+                                print(f"Virus found inside zip file \"{true_path}\": {os.path.basename(path)}")
+                        case 'file':
+                            if await md5_scan(file):
+                                print(f"Virus found: {true_path}")
         except Exception as e:
             logger.error(f"Error scanning file {path}: {str(e)}") # Permissions probably
 
+
+#################################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help='Path to the file or directory to scan')
     parser.add_argument('--num_threads', type=int, default=os.cpu_count(), help='Number of threads')
-    parser.add_argument('--mode', type=str, default='all', choices=['all', 'md5', 'zip'], help='Scan Mode')
     parser.add_argument('--verbose', action='store_true', help='Set verbose mode')
     parser.add_argument('--bypass_checks', action='store_true', help='Bypasses validation checks')
     parser.add_argument('--scheduling', type=str, default='block', choices=['block', 'cycle'], help='Scheduling Type')
@@ -145,18 +157,45 @@ if __name__ == '__main__':
         await init_database(args.bypass_checks)
         logger.info(f"using {args.num_threads} threads")
 
+        # If path is a directory
         if os.path.isdir(args.path):
             logger.info(f"{args.path} is a directory")
             if args.scheduling == "block":
                 logger.info(f"using block scheduling")
-                await scan_dir(args.path, args.num_threads, args.mode, asyncio.Semaphore(args.num_threads))
+                await scan_dir_with_block(args.path, args.num_threads, 'dir', asyncio.Semaphore(args.num_threads), args.path)
             else:
                 logger.info(f"using cycle scheduling")
-                await scan_dir_with_cycle_scheduling(args.path, args.num_threads, args.mode)
-        elif os.path.isfile(args.path):  # One file, one thread
+                await scan_dir_with_cycle_scheduling(args.path, args.num_threads, 'dir', args.path)
+
+        # If path is a zip file
+        elif zipfile.is_zipfile(args.path):
+            logger.info(f"{args.path} is a zip file")
+            tmp_dir = tempfile.mkdtemp()
+            is_extracted = extract_zip(args.path, tmp_dir)
+
+            if is_extracted:
+                logger.info(f"Scanning extracted files in TMP directory: {tmp_dir}")
+                
+                if args.scheduling == "block":
+                    logger.info(f"using block scheduling")
+                    await scan_dir_with_block(tmp_dir, args.num_threads, 'zip', asyncio.Semaphore(args.num_threads), args.path)
+                else:
+                    logger.info(f"using cycle scheduling")
+                    await scan_dir_with_cycle_scheduling(tmp_dir, args.num_threads, 'zip', args.path)
+
+                try:
+                    shutil.rmtree(tmp_dir)
+                    logger.info(f"Deleted TMP directory: {tmp_dir}")
+                except Exception as e:
+                    logger.error(f"Error deleting TMP directory: {e}")
+
+        # If path is an individual file
+        elif os.path.isfile(args.path):
             logger.info(f"{args.path} is a file")
-            await scan_file(args.path, args.mode, asyncio.Semaphore(1))
-        else:  # Invalid path
+            await scan_file(args.path, 'file', asyncio.Semaphore(1), args.path)
+
+        # If path is invalid
+        else:
             logger.error(f"{args.path} is not a directory or file")
             print('Invalid path')
 
